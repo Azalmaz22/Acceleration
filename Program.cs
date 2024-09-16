f6a7a25a029cf4f4ba0e13d0cfb93daf4077219d
Azalmaz22 <Damir2298@yandex.ru>
2024-09-16 16:10:09 +0300

    Racing

--- Program.cs
+++ Program.cs
@@ -6,23 +6,19 @@
 
         static void Main(string[] args)
         {
-            var myCar = new Car();
+            var myCar = new  Car("BMW", 55);
+         
             myCar.Speed = 0;
             Console.WriteLine("Racing!");
 
             Console.WriteLine("Enter you car name");
 
             var answer1 = Console.ReadLine();
-            if (answer1 == "BMW")
-            {
-                myCar.Name = answer1;
-                
-                
-            }
+            
             Console.WriteLine("Current speed is GMPH");
             while (true)
             {
-
+                
                 Console.WriteLine("Enter direction");
                 string answer = Console.ReadLine();
                 var number = int.Parse(answer);
@@ -45,8 +41,7 @@
                 var answer2 = Console.ReadLine();
                 if (answer2 == "no")
                 {
-                    myCar.Speed = myCar.Speed ;
-                    
+                  
                     continue;
                    
                 }
@@ -61,9 +56,15 @@
     }
     public class Car
     {
+       
         public string Name;
         public int Speed;
-
+        public Car(string Name, int Speed)
+        { 
+            Name = "BMW";
+            Speed = 55;
+        }
+       
         public void PrintState() 
         {
             Console.WriteLine("{0} is going {1} MPH.", Name, Speed);
